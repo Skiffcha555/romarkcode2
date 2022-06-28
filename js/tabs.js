88a453c35cd396008aa517dbe8b49b240439953d
Dastan Kudaibergen <dkudaybergen@gmail.com>
2022-06-28 20:17:32 +0600

    add

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,11 @@
 function openCategory(evt, sliderCategory) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
+
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
